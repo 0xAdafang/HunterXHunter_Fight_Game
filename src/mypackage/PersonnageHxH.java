d80220c173359fb8feffe59821993326da063c17
Adafang <adafang@proton.me>
2024-10-08 17:01:26 -0400

    Update PersonnageHxH.java

--- src/mypackage/PersonnageHxH.java
+++ src/mypackage/PersonnageHxH.java
@@ -9,9 +9,19 @@ public class PersonnageHxH
     private String nen;
     private int experience;
 
+    private String capaciteOffensive;
+    private String capaciteDefensive;
+    private String capaciteEsquive;
+
+    private int cooldownOffensive = 0; // Tour actuel pour la capacité offensive
+    private int cooldownDefensive = 0; // Tour actuel pour la capacité défensive
+    private int cooldownEsquive = 0; // Tour actuel pour la capacité d'esquive
+
+
      // Constructeur de la classe 
 
-     public PersonnageHxH (String nom, int vie, int force, int vitesse, int intelligence, String nen, int experience)
+     public PersonnageHxH (String nom, int vie, int force, int vitesse, int intelligence, String nen, int experience, 
+     String capaciteOffensive, String capaciteDefensive, String capacitéEsquive)
      {
         this.nom = nom;
         this.vie = vie;
@@ -20,6 +30,10 @@ public class PersonnageHxH
         this.intelligence = intelligence;
         this.nen = nen;
         this.experience = experience;
+
+        this.capaciteOffensive = capaciteOffensive;
+        this.capaciteDefensive =capaciteDefensive;
+        this.capaciteEsquive = capacitéEsquive;
      }
 
      // Getters pour acceder aux attributs
@@ -51,12 +65,31 @@ public class PersonnageHxH
      {
         return experience;
      }
+
+     public void setVie(int vie) 
+     {
+         this.vie = vie;
+     }
+     public String getCapaciteOffensive()
+     {
+         return capaciteOffensive;
+     }
+     public String getCapaciteDefensive()
+     {
+         return capaciteDefensive;
+     }
+     public String getCapaciteEsquive()
+     {
+         return capaciteEsquive;
+     }
      //Methode afficher info personnage
      public void afficherInfo()
      {
         System.out.println("Nom : " + nom + ", Vie : " + vie + ", Force : " + force + ", Vitesse : " + vitesse + ", Intelligence : " + intelligence + ", Catégorie de Nen : " + nen + ", Exeperience : " + experience);
      }
-
-
-
-}
+     public void reductionCooldown()
+     {
+      if (cooldownOffensive > 0 ) cooldownOffensive--;
+      if (cooldownDefensive > 0 ) cooldownDefensive--;
+      if (cooldownEsquive > 0) cooldownEsquive--;
+     }
