1bbe18d6a531746bea293676f2d1ea3ac309492b
Adafang <adafang@proton.me>
2024-10-11 16:01:43 -0400

    Add files via upload

--- src/mypackage/CapaciteOffensive.java
+++ src/mypackage/CapaciteOffensive.java
@@ -2,45 +2,53 @@ package mypackage;
 
 import java.util.Random;
 
-public class CapaciteOffensive extends CapaciteNen
+public class CapaciteOffensive extends CapaciteNen 
 {
-    public CapaciteOffensive(String nom, int cooldown)
+    public CapaciteOffensive(String nom, int cooldown) 
     {
         super(nom, cooldown);
     }
+
     @Override
-    public int activer(PersonnageHxH attaquant, PersonnageHxH defenseur, boolean coupCritique)
+    public int activer(PersonnageHxH attaquant, PersonnageHxH defenseur, boolean coupCritique) 
     {
         int degats = attaquant.getForce();
 
-        if(nom.equals("Pierre-feuille-Ciseaux"))
+        if (nom.equals("Pierre-feuille-Ciseaux")) 
         {
             degats *= coupCritique ? 3 : 2;
-        }
-        else if(nom.equals("Godspeed"))
+        } else if (nom.equals("Godspeed")) 
         {
             degats *= coupCritique ? 4 : 2;
         }
-        System.out.println(attaquant.getNom() + "utilise " + nom + "pour " + degats + " dégâts!");
-        resetCooldown();
-        return degats;
+
+        System.out.println(attaquant.getNom() + " utilise " + nom + " pour " + degats + " dégâts!");
+
+        resetCooldown();  // Reset cooldown après utilisation
+        return degats;  // Retourne les dégâts pour être appliqués au défenseur
     }
 
-    public boolean calculerCoupCritique (int intelligence, boolean coupCritiqueInitial)
+    public boolean calculerCoupCritique(int intelligence, boolean coupCritiqueInitial) 
     {
-         // Ajout d'une probabilité supplémentaire de coup critique basée sur l'intelligence (ex. +1% par point d'intelligence)
         Random random = new Random();
         int chanceCritique = random.nextInt(100);
-        int SeuilCritique = 10; //10% de base
+        int seuilCritique = 10;  // 10% de base pour un coup critique
 
-        // Ajuste la probabilité selon l'intelligence du personnage
-        if (chanceCritique < SeuilCritique + intelligence / 10)
+        if (chanceCritique < seuilCritique + intelligence / 10) 
         {
-            return true; //coup critique reussi !
+            return true;  // Coup critique réussi !
         }
-        return coupCritiqueInitial; //Sinon, conserve la valeur initiale
+        return coupCritiqueInitial;  // Sinon, conserve la valeur initiale
+    }
+    public int getCooldown() 
+    {
+        return cooldown; // Retourne le cooldown actuel
+    }
 
-
-
+    public void decrementerCooldown()
+    {
+        if (cooldown > 0) {
+            cooldown--;
+        }
     }
 }
